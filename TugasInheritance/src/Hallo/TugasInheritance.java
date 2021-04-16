6341045ea3c37e880af3ebc895fc459546a6f464
TalithaAurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-04-16 13:17:25 +0700

    Add files via upload

---
+++ TugasInheritance/src/Hallo/TugasInheritance.java
@@ -0,0 +1,18 @@
+// Nama  : Talitha Aurelia Shakira
+// NIM   : 11200930000023
+// kelas : 2A
+
+package Hallo;
+public class TugasInheritance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("HaloIndonesia");
+     System.out.println("Selamat Pagi Indonesia");
+     System.out.println("Jaya Indonesia");
+ }
+}
+  
+    
